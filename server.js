bc92ead0a94cdc7cd36b370539f5d960ad6f7253
unknown <you@example.com>
2025-01-15 00:07:06 +0500

    new

--- server.js
+++ server.js
@@ -42,6 +42,16 @@ io.on('connection', (socket) => {
         io.to(peerId).emit('iceCandidate', { peerId: socket.id, candidate });
     });
 
+    // Start the game when the 'startGame' event is received
+    socket.on('startGame', ({ roomId }) => {
+        console.log(`Starting game in room: ${roomId}`);
+
+        // Notify all players in the room that the game is starting
+        io.to(roomId).emit('gameStarted', { message: 'The game has started!' });
+
+        // Optionally, you can reset or initialize any game state here
+    });
+
     // When a player disconnects, clean up
     socket.on('disconnect', () => {
         console.log('User disconnected:', socket.id);
