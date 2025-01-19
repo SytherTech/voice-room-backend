0d9f8785f6f2f3e7a88827f6c26a7ec6852b1d4f
unknown <you@example.com>
2025-01-19 20:13:04 +0500

    new

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ io.on('connection', (socket) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
